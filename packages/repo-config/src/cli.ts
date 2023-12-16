08b3e9c81526d6dade54d67e6aeaf7e1bff94f8b
Eric Mika <eric@ericmika.com>
2023-12-16 18:23:56 -0500

    Migrate to fs-extra.

--- packages/repo-config/src/cli.ts
+++ packages/repo-config/src/cli.ts
@@ -1,50 +1,49 @@
 #!/usr/bin/env node
-import { buildCommands } from '../../../src/command-builder.js';
-import { checkFileExists } from '../../../src/file-utils.js';
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { packageUp } from 'package-up';
+import { buildCommands } from '../../../src/command-builder.js'
+import fse from 'fs-extra'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { packageUp } from 'package-up'
 
 await buildCommands('repo-config', '[Repo Config]', 'gray', {
 	init: {},
 	printConfig: {
 		async command(logStream) {
-			const destinationPackage = await packageUp();
+			const destinationPackage = await packageUp()
 			if (destinationPackage === undefined) {
 				logStream.write(
 					'Error: The `--print-config` flag must be used in a directory with a package.json file somewhere above it\n',
-				);
-				return 1;
+				)
+				return 1
 			}
 
-			const sourcePackage = await packageUp({ cwd: fileURLToPath(import.meta.url) });
+			const sourcePackage = await packageUp({ cwd: fileURLToPath(import.meta.url) })
 			if (sourcePackage === undefined) {
-				logStream.write('Error: The script being called was not in a package, weird.\n');
-				return 1;
+				logStream.write('Error: The script being called was not in a package, weird.\n')
+				return 1
 			}
 
-			const sourceDirectory = path.join(path.dirname(sourcePackage), 'init/');
-			const destinationDirectory = path.dirname(destinationPackage);
+			const sourceDirectory = path.join(path.dirname(sourcePackage), 'init/')
+			const destinationDirectory = path.dirname(destinationPackage)
 
-			let exitCode = 0;
+			let exitCode = 0
 
-			for (const file of await fs.readdir(sourceDirectory)) {
-				const destinationPath = path.join(destinationDirectory, file);
+			for (const file of await fse.readdir(sourceDirectory)) {
+				const destinationPath = path.join(destinationDirectory, file)
 
 				// Merge with existing, if present
-				if (await checkFileExists(destinationPath)) {
-					const fileContent = await fs.readFile(destinationPath, 'utf8');
-					logStream.write(`💾 Contents of "${file}":\n`);
-					logStream.write(fileContent);
-					logStream.write('\n');
+				if (await fse.exists(destinationPath)) {
+					const fileContent = await fse.readFile(destinationPath, 'utf8')
+					logStream.write(`💾 Contents of "${file}":\n`)
+					logStream.write(fileContent)
+					logStream.write('\n')
 				} else {
-					logStream.write(`Error: Could not find ${file}\n`);
-					exitCode = 1;
+					logStream.write(`Error: Could not find ${file}\n`)
+					exitCode = 1
 				}
 			}
 
-			return exitCode;
+			return exitCode
 		},
 	},
-});
+})
