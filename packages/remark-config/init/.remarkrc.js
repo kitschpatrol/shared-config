800f2202e5c68316608509073bae67c2aa579234
Eric Mika <eric@ericmika.com>
2024-06-14 14:59:30 -0400

    Additional remakr override examples.

--- packages/remark-config/init/.remarkrc.js
+++ packages/remark-config/init/.remarkrc.js
@@ -3,7 +3,10 @@ import sharedConfig, { overrideRules } from '@kitschpatrol/remark-config'
 const localConfig = {
 	...sharedConfig,
 	// Overrides are a special case, working as below (set `false` as the second element to disable):
-	// plugins: overrideRules(sharedConfig.plugins, [['remark-lint-first-heading-level', 2]])
+	// plugins: overrideRules(sharedConfig.plugins, [
+	// 	['remark-lint-first-heading-level', 2],
+	// 	['remarkValidateLinks', { repository: false }],
+	// ]),
 }
 
 export default localConfig
