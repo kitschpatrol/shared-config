1c61107bd60129fc7242a031569b625a3b0d3fc0
Eric Mika <eric@ericmika.com>
2025-11-03 22:25:56 -0500

    Update eslint rules.

--- packages/eslint-config/src/presets/test.ts
+++ packages/eslint-config/src/presets/test.ts
@@ -3,13 +3,13 @@ import type { Rules } from '../types'
 export const testRecommendedRules: Rules = {
 	// Begin expansion '@vitest/eslint-plugin' 'recommended'
 	'test/expect-expect': 'error',
-	'test/no-identical-title': 'error',
 	'test/no-commented-out-tests': 'error',
-	'test/valid-title': 'error',
-	'test/valid-expect': 'error',
-	'test/valid-describe-callback': 'error',
-	'test/require-local-test-context-for-concurrent-snapshots': 'error',
+	'test/no-identical-title': 'error',
 	'test/no-import-node-test': 'error',
 	'test/prefer-called-exactly-once-with': 'error',
+	'test/require-local-test-context-for-concurrent-snapshots': 'error',
+	'test/valid-describe-callback': 'error',
+	'test/valid-expect': 'error',
+	'test/valid-title': 'error',
 	// End expansion
 }
